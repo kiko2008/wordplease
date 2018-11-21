4f2c124bdf53491566e18b424f951d239b553b8d
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-21 23:17:16 +0100

    Pagina de blogs del sitio web

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,10 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from blogs.api import BlogViewSet
+from blogs.views import BlogsView
 
 router = DefaultRouter()
 router.register('blogs', BlogViewSet)
 
 urlpatterns = [
+    path('blogs', BlogsView.as_view(), name='blogs'),
+
+    # API REST
     path('api/1.0/', include(router.urls)),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from blogs.models import Blog
+
+
+class BlogsView(View):
+
+    def get(self, request):
+        all_blogs = Blog.objects.select_related('user').all()
+
+        return render(request, 'blogs.html', {'list_all_blogs': all_blogs})
