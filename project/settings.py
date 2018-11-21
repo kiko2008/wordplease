1f4bdd2dffcf6864a79b74945223df521b0bf37b
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-21 23:21:06 +0100

    Configuracion para el sitio web

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'general_templates',
     'blogs',
     'posts',
     'users',
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/uploads/'
 
 # debug and logging configuration
 LOGGING = {
