58e105af387280a4f9a8aa113b036dd80e45fc9d
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-22 22:59:46 +0100

    Pagina de blogs y detalle de blog

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from blogs.api import BlogViewSet
-from blogs.views import BlogsView
+from blogs.views import BlogsView, BlogDetailView
 
 router = DefaultRouter()
 router.register('blogs', BlogViewSet)
 
 urlpatterns = [
     path('blogs', BlogsView.as_view(), name='blogs'),
+    path('blogs/<pk>/', BlogDetailView.as_view(), name='detail_blog'),
 
     # API REST
     path('api/1.0/', include(router.urls)),
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import ListView, DetailView
 
 from blogs.models import Blog
+from posts.models import Post
 
 
-class BlogsView(View):
+class BlogsView(ListView):
 
-    def get(self, request):
-        all_blogs = Blog.objects.select_related('user').all()
+    template_name = 'blogs.html'
+    queryset = Blog.objects.select_related('user').all()
+    context_object_name = 'list_all_blogs'
 
-        return render(request, 'blogs.html', {'list_all_blogs': all_blogs})
+
+class BlogDetailView(DetailView):
+
+    model = Blog
+    template_name = 'detail_blog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['list_posts'] = Post.objects.filter(blog_id = context.get('object').id).order_by('pub_date')
+        return context
+
