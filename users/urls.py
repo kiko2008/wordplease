3a1af67d457619ef20c302e13e3618def2fb9ab3
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-22 23:03:57 +0100

    Pagina de login y logout de usurios

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.api import UserViewSet
+from users.views import LoginView, LogoutView
 
 router = DefaultRouter()
 router.register('users', UserViewSet, base_name='users')
 
 urlpatterns = [
+
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    #path('register', RegisterView.as_view(), name='register'),
+
+    # API REST
     path('api/1.0/', include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as login_user_in_session, logout as destroy_user_session
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, request):
+
+        return render(request, 'login.html')
+
+    def post(self, request):
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            messages.error(request, 'Usuario o password incorrectos!')
+        else:
+            login_user_in_session(request, user)
+            return redirect(request.GET.get('next', 'home'))
+
+        return render(request, 'login.html')
+
+
+class LogoutView(View):
+
+    def get(self, request):
+        destroy_user_session(request)
+        return redirect('home')
