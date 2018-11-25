5d8532639a7f99d12dce1cce924848260d323063
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-25 17:37:42 +0100

    Anadiendo Url de los blogs

--- blogs/serializer.py
+++ blogs/serializer.py
@@ -17,12 +17,17 @@ class BlogListSerializer(serializers.ModelSerializer):
 
     user = UserSerializer(read_only=True)
     count_posts = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
 
     class Meta:
 
         model = Blog
-        fields = ('user', 'title', 'description', 'count_posts')
+        fields = ('user', 'title', 'description', 'count_posts', 'url')
 
     def get_count_posts(self, obj):
         count_posts =  Post.objects.filter(blog_id=obj.pk).count()
         return count_posts
+
+    def get_url(self, obj):
+        url = 'http://localhost:8000/blogs/{0}'.format(obj.pk)
+        return url
